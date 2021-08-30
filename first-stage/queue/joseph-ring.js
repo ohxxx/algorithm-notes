bc226d65b27b48e52797d6544d40c0824c25eade
humengming <humengmingxx@gmail.com>
2021-08-30 23:00:44 +0800

    feat(queue)：add joseph-ring example

---
+++ first-stage/queue/joseph-ring.js
@@ -0,0 +1,53 @@
+// 队列 - 暂时忽略队列为空情况
+class Queue {
+  constructor() {
+    this.count = 0
+    this.lowestCount = 0
+    this.items = {}
+  }
+
+  // 入队
+  enqueue(element) {
+    this.items[this.count] = element
+    this.count++
+  }
+
+  // 出队
+  dequeue() {
+    const temp = this.items[this.lowestCount]
+    delete this.items[this.lowestCount]
+    this.lowestCount++
+    return temp
+  }
+
+  // 队列长度
+  size() {
+    return this.count - this.lowestCount
+  }
+}
+
+function JosephRing(arr) {
+  const queue = new Queue()
+
+  for (let i = 0; i < arr.length; i++) {
+    queue.enqueue(arr[i])
+  }
+
+  let idx = 0
+  while (queue.size() > 1) {
+    const temp = queue.dequeue() // 先出队
+    idx++
+    if (idx % 3 !== 0) { // 判断是否符合出队条件，不符合入队
+      queue.enqueue(temp)
+    }
+  }
+
+  return queue.dequeue()
+}
+
+// test
+let arr = []
+for (let i = 0; i < 100; i++) {
+  arr.push(i)
+}
+console.log(JosephRing(arr));
