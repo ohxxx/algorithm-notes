dd42372439989b9a3f18e3a0ed2e7af1683aef70
humengming <humengmingxx@gmail.com>
2021-10-16 17:20:27 +0800

    feat(dp)：add lcs example

---
+++ first-stage/algorithm-design/dynamic-programming/longest-common-subsequence.js
@@ -0,0 +1,35 @@
+const lcs = (wordX, wordY) => {
+  const m = wordX.length;
+  const n = wordY.length;
+  const l = [];
+
+  for (let i = 0; i <= m; i++) {
+    l[i] = [];
+    for (let j = 0; j <= n; j++) {
+      l[i][j] = 0;
+    }
+  }
+
+  for (let i = 0; i <= m; i++) {
+    for (let j = 0; j <= n; j++) {
+      if (i === 0 || j === 0) {
+        l[i][j] = 0;
+      } else if (wordX[i - 1] === wordY[j - 1]) {
+        l[i][j] = l[i - 1][j - 1] + 1;
+      } else {
+        const a = l[i - 1][j];
+        const b = l[i][j - 1];
+        l[i][j] = a > b ? a : b;
+      }
+    }
+  }
+
+  return l[m][n];
+}
+
+
+// test
+const wordX = 'acbaed';
+const wordY = 'abcadf';
+
+console.log('lcs', lcs(wordX, wordY));
